1f35b5afceabb91d8686d480c6a0e73438bf99d0
Christoffer Thygesen <stofferdude@hotmail.com>
2016-12-24 03:41:55 +0100

    omg it's finished

--- SelfCreatingMaze/SelfCreatingMaze/Cell.cs
+++ SelfCreatingMaze/SelfCreatingMaze/Cell.cs
@@ -1,32 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SelfCreatingMaze
 {
     class Cell
     {
-        public int i { get; set; }
-        public int j { get; set; }
+        public int i { get; set; } //x
+        public int j { get; set; } //y
+        public bool[] walls = { true, true, true, true }; // t, r, b, l
+        public bool IsVisited = false;
 
-        //public bool[] walls = { true, true, true, true }; // t, r, b, l
 
+        //constructor
         public Cell(int i, int j)
         {
             this.i = i;
             this.j = j;
         }
-
-        private bool[] walls = { true, true, true, true };
-
-        public bool[] MyWalls
-        {
-            get { return walls; }
-            set { walls = value; }
-        }
-
     }
 }
--- SelfCreatingMaze/SelfCreatingMaze/Form1.cs
+++ SelfCreatingMaze/SelfCreatingMaze/Form1.cs
@@ -1,29 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SelfCreatingMaze
 {
     public partial class Form1 : Form
     {
-        Bitmap drawingArea = new Bitmap(401, 401);
+        
+        Bitmap drawingArea = new Bitmap(403, 403);
         int cols, rows;
         int w = 40;
         List<Cell> grid = new List<Cell>();
+        private Graphics g;
+
+        private Cell current;
 
         public Form1()
         {
             InitializeComponent();
+            InitGrid();
         }
 
-
-        private void Form1_Load(object sender, EventArgs e)
+        //sets up the grid
+        public void InitGrid()
         {
             cols = drawingArea.Width / w;
             rows = drawingArea.Height / w;
@@ -36,37 +38,183 @@ namespace SelfCreatingMaze
                     grid.Add(cell);
                 }
             }
+
+            current = grid[0];          
+        }
+
+
+        private List<Cell> stack = new List<Cell>();
+        //paint screen "update"
+        public void PaintToScreen()
+        {
+            g = Graphics.FromImage(drawingArea);
+            for (int i = 0; i < grid.Count; i++)
+            {
+                MakeWalls(grid[i]);            
+            }
+
+            current.IsVisited = true;
+
+            //step 1
+            Cell next = CheckNeigbors(current);
+            
+            if (next != null)
+            {                
+                next.IsVisited = true;
+
+                //step 2
+                stack.Add(current);
+
+                //sted 3
+                RemoveWalls(current, next);
+
+                //step 4
+                current = next;
+            }
+            else if (stack.Count > 0)
+            {
+                current = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+            }
+
+            pictureBox1.Image = drawingArea;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
+            Thread.Sleep(500);
+            PaintToScreen();
+        }
 
-            Graphics g = Graphics.FromImage(drawingArea);
+        //remove walls, so when updated they are not painted again
+        private void RemoveWalls(Cell theCurrent, Cell theNext)
+        {
+            int x = theCurrent.i - theNext.i;
+            int y = theCurrent.j - theNext.j;
 
-            for (int i = 0; i < grid.Count; i++)
+            if (x == 1)
             {
-                int x = grid[i].i * w;
-                int y = grid[i].j * w;
+                theCurrent.walls[3] = false;
+                theNext.walls[1] = false;
+            }
+            else if (x == -1)
+            {
+                theCurrent.walls[1] = false;
+                theNext.walls[3] = false;
+            }
 
-                if (grid[i].MyWalls[0])
+            if (y == 1)
+            {
+                theCurrent.walls[0] = false;
+                theNext.walls[2] = false;
+            }
+            else if (y == -1)
+            {
+                theCurrent.walls[2] = false;
+                theNext.walls[0] = false;
+            }
+        }
+
+        //indexes the list of Cells
+        private int Index(int i, int j)
+        {
+            if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1)
+            {
+                return -1;
+            }
+            return i + j * cols;
+        }
+
+        //check who's around and visisted       
+        Random rnd = new Random();
+        private Cell CheckNeigbors(Cell current)
+        {
+            List<Cell> neighbors = new List<Cell>();
+
+            if (Index(current.i, current.j - 1) > -1)
+            {
+                Cell top = grid[Index(current.i, current.j - 1)];
+
+                if (top != null && !top.IsVisited)
                 {
-                    g.DrawLine(new Pen(Color.Black), x, y, x + w, y);
-                }
-                if (grid[i].MyWalls[1])
-                {
-                    g.DrawLine(new Pen(Color.Black), x + w, y, x + w, y + w);
-                }
-                if (grid[i].MyWalls[2])
-                {
-                    g.DrawLine(new Pen(Color.Black), x + w, y + w, x, y + w);
-                }
-                if (grid[i].MyWalls[3])
-                {
-                    g.DrawLine(new Pen(Color.Black), x, y + w, x, y);
+                    neighbors.Add(top);
                 }
             }
 
-            pictureBox1.Image = drawingArea;
+            if (Index(current.i + 1, current.j) > -1)
+            {
+                Cell right = grid[Index(current.i + 1, current.j)];
+
+                if (right != null && !right.IsVisited)
+                {
+                    neighbors.Add(right);
+                }
+            }
+
+            if (Index(current.i, current.j + 1) > -1)
+            {
+                Cell bottom = grid[Index(current.i, current.j + 1)];
+
+                if (bottom != null && !bottom.IsVisited)
+                {
+                    neighbors.Add(bottom);
+                }
+            }
+
+            if (Index(current.i - 1, current.j) > -1)
+            {
+                Cell left = grid[Index(current.i - 1, current.j)];
+
+                if (left != null && !left.IsVisited)
+                {
+                    neighbors.Add(left);
+                }
+            }
+              
+
+            if (neighbors.Count > 0)
+            {
+                int r = rnd.Next(0, neighbors.Count);
+                return neighbors[r];
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        //init grid building, make Trump proud!
+        private void MakeWalls(Cell c)
+        {
+            int x = c.i * w;
+            int y = c.j * w;
+
+            if (c.walls[0])
+            {
+                g.DrawLine(new Pen(Color.White, 3), x, y, x + w, y);
+            }
+            if (c.walls[1])
+            {
+                g.DrawLine(new Pen(Color.White, 3), x + w, y, x + w, y + w);
+            }
+            if (c.walls[2])
+            {
+                g.DrawLine(new Pen(Color.White, 3), x + w, y + w, x, y + w);
+            }
+            if (c.walls[3])
+            {
+                g.DrawLine(new Pen(Color.White, 3), x, y + w, x, y);
+            }
+
+            if (c.IsVisited)
+            {
+                g.FillRectangle(new SolidBrush(Color.Blue), x, y, w, w);
+            }
         }
     }
 }
