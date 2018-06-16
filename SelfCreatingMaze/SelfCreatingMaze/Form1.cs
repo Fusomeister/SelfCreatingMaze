97c6b8066e6f3deaf9d2e9a3d012d3979deafae5
Christoffer <stofferdude@hotmail.com>
2018-06-16 04:59:42 +0200

    UI

--- SelfCreatingMaze/SelfCreatingMaze/Form1.cs
+++ SelfCreatingMaze/SelfCreatingMaze/Form1.cs
@@ -11,18 +11,22 @@ namespace SelfCreatingMaze
     {
 
         private Bitmap drawingArea = new Bitmap(403, 403);
+        private Bitmap drawingArea2 = new Bitmap(403, 403);
         private int cols, rows;
-        private int w = 20;
+        private int w = 10;
+        private int delay = 10;
         private List<Cell> grid = new List<Cell>();
         private List<Cell> stack = new List<Cell>();
         private Graphics g;
         private Random rnd = new Random();
         private Cell current;
+        private bool isStarted = false;
 
         public Form1()
         {
             InitializeComponent();
-            InitGrid();
+            textBox1.Text = w + "";
+            textBox2.Text = delay + "";
         }
 
         //sets up the grid
@@ -39,51 +43,50 @@ namespace SelfCreatingMaze
                     grid.Add(cell);
                 }
             }
-
             current = grid[0];          
         }
         
         //paint screen "update"
         public void PaintToScreen()
         {
-            g = Graphics.FromImage(drawingArea);
-            for (int i = 0; i < grid.Count; i++)
-            {
-                MakeWalls(grid[i]);            
+            if(isStarted==true)
+            {           
+                g = Graphics.FromImage(drawingArea);
+
+                //for (int i = 0; i < grid.Count; i++)
+                //{
+                //    MakeWalls(grid[i]);
+                //}
+                
+                current.IsVisited = true;
+                Highlighted(current);
+                MakeWalls(current);
+
+                //step 1
+                Cell next = CheckNeigbors(current);
+
+                if (next != null)
+                {                
+                    next.IsVisited = true;
+                    Highlighted(next);
+                    //step 2
+                    stack.Add(current);
+
+                    //sted 3
+                    RemoveWalls(current, next);
+
+                    //step 4
+                    current = next;
+                }
+                else if (stack.Count > 0)
+                {
+                    current = stack[stack.Count - 1];
+                    stack.RemoveAt(stack.Count - 1);
+                    Highlighted(current);
+                }
+
+                pictureBox1.Image = drawingArea;               
             }
-
-            current.IsVisited = true;
-            Highlighted(current);
-
-            //step 1
-            Cell next = CheckNeigbors(current);
-            
-            if (next != null)
-            {                
-                next.IsVisited = true;
-
-                //step 2
-                stack.Add(current);
-
-                //sted 3
-                RemoveWalls(current, next);
-
-                //step 4
-                current = next;
-            }
-            else if (stack.Count > 0)
-            {
-                current = stack[stack.Count - 1];
-                stack.RemoveAt(stack.Count - 1);
-            }
-
-            pictureBox1.Image = drawingArea;
-        }
-
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
-        {
-            Thread.Sleep(10);
-            PaintToScreen();
         }
 
         //remove walls, so when update they are not painted again
@@ -126,7 +129,6 @@ namespace SelfCreatingMaze
         }
 
         //check who's around and visisted       
-
         private Cell CheckNeigbors(Cell current)
         {
             List<Cell> neighbors = new List<Cell>();
@@ -206,10 +208,64 @@ namespace SelfCreatingMaze
             {
                 g.DrawLine(new Pen(Color.White, 3), x, y + w, x, y);
             }
-
             if (c.IsVisited)
             {
                 g.FillRectangle(new SolidBrush(Color.Blue), x, y, w, w);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isStarted = true;
+            InitGrid();
+            pictureBox1.Refresh();
+            //PaintToScreen();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+                Thread.Sleep(delay);
+                PaintToScreen();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Image = null;
+            isStarted = false;
+            drawingArea = new Bitmap(403, 403);
+            g = Graphics.FromImage(drawingArea);
+            pictureBox1.Invalidate();
+            grid.Clear();
+            stack.Clear();
+
+            InitGrid();           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (isStarted) { }
+            else
+            {
+                bool isNum = int.TryParse(textBox1.Text, out int n);
+
+                if (isNum)
+                {
+                    w = int.Parse(textBox1.Text);
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (isStarted) { }
+            else
+            {
+                bool isNum = int.TryParse(textBox2.Text, out int n);
+
+                if (isNum)
+                {
+                    delay = int.Parse(textBox2.Text);
+                }
             }
         }
 
@@ -218,7 +274,7 @@ namespace SelfCreatingMaze
             int x = c.i * w;
             int y = c.j * w;
 
-            g.FillRectangle(new SolidBrush(Color.BlueViolet), x, y, w, w);
+            g.FillRectangle(new SolidBrush(Color.Green), x, y, w, w);
         }
     }
 }
